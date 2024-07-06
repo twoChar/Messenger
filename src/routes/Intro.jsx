4e33e569b2b8f975ee2af739eb6da6245a6a8722
Tushar Suredia <tucy254@gmail.com>
2024-07-07 02:40:13 +0530

    Updated

--- src/routes/Intro.jsx
+++ src/routes/Intro.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 function Intro({ isContactPage }) {
   return (
     <div className="intro-page">
-      <img src="/jam.png" alt="" />
+      {/* <img src="/jam.png" alt="" /> */}
       <p>
         {isContactPage
           ? 'Select a contact to start a conversation'
-          : 'Welcome to Cherry Chat! Select a chat or start a new conversation'}
+          : 'Welcome to Messenger App! Select a chat or start a new conversation'}
       </p>
     </div>
   );
